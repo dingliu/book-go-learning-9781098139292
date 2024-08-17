0171fad417806e75f187840732ba1be16fd0e26a
Ding Liu <contact@dingliu.me>
2024-08-17 19:59:24 -0400

    add integer type conversions example

--- ch2/listings/types-literals.go
+++ ch2/listings/types-literals.go
@@ -8,4 +8,9 @@ func main() {
 	var sum1 float64 = float64(x) + y
 	var sum2 int = x + int(y)
 	fmt.Println(sum1, sum2)
+
+	var b byte = 100
+	var sum3 int = x + int(b)
+	var sum4 byte = byte(x) + b
+	fmt.Println(sum3, sum4)
 }
