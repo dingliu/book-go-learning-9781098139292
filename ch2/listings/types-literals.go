5c549c7a8ce84ea4e8eccf8bfc61d37e1f765043
Ding Liu <contact@dingliu.me>
2024-08-17 21:23:06 -0400

    var declare

--- ch2/listings/types-literals.go
+++ ch2/listings/types-literals.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+var (
+	x    int
+	y        = 20
+	z    int = 30
+	d, e     = 40, "hello"
+	f, g string
+)
+
 func main() {
 	var x int = 10
 	var y float64 = 30.2
@@ -13,11 +21,19 @@ func main() {
 	var sum3 int = x + int(b)
 	var sum4 byte = byte(x) + b
 	fmt.Println(sum3, sum4)
+
 	untypedLiterals()
+	varDeclareWithinFunc()
 }
 
 func untypedLiterals() {
 	var x float64 = 10
 	var y float64 = 200.3 * 5
+	fmt.Println(x, y)
+}
+
+func varDeclareWithinFunc() {
+	x := 10
+	x, y := 20, "hello"
 	fmt.Println(x, y)
 }
