52b626c79d44b029c13a652791be086adc9999fa
Ding Liu <contact@dingliu.me>
2024-08-18 14:11:02 -0400

    compare zero value of slice with nil

--- ch3/listings/composite_types.go
+++ ch3/listings/composite_types.go
@@ -4,10 +4,16 @@ import "fmt"
 
 func main() {
 	compareArrays()
+	zeroSliceIsNil()
 }
 
 func compareArrays() {
 	var x = [...]int{1, 2, 3}
 	var y = [3]int{1, 2, 3}
 	fmt.Println(x == y)
+}
+
+func zeroSliceIsNil() {
+	var x []int
+	fmt.Println(x == nil)
 }
