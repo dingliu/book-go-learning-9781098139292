2433c91a8de574c117d42c3bcdc4cf6fedc89f3c
Ding Liu <contact@dingliu.me>
2025-09-06 16:39:12 -0400

    Update function exercises

---
+++ ch5/exercises/ch5_exercises.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+)
+
+// exercise 1
+
+func add(i, j int) (int, error) { return i + j, nil }
+func sub(i, j int) (int, error) { return i - j, nil }
+func mul(i, j int) (int, error) { return i * j, nil }
+func div(i, j int) (int, error) {
+	if j == 0 {
+		return 0, errors.New("Division by zero")
+	}
+	return i / j, nil
+}
+
+var opMap = map[string]func(int, int) (int, error){
+	"+": add,
+	"-": sub,
+	"*": mul,
+	"/": div,
+}
+
+func main() {
+	expressions := [][]string{
+		{"2", "+", "3"},
+		{"2", "-", "3"},
+	}
+
+	for _, exp := range expressions {
+		if len(exp) != 3 {
+			fmt.Println("Invalid expression:", exp)
+		}
+		p1, err := strconv.Atoi(exp[0])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		op := exp[1]
+		opFunc, ok := opMap[op]
+		if !ok {
+			fmt.Println("Unsupported operator:", op)
+		}
+		p2, err := strconv.Atoi(exp[2])
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		result, err := opFunc(p1, p2)
+		if err != nil {
+			fmt.Println("Got error:", err)
+		}
+		fmt.Println(result)
+	}
+}
